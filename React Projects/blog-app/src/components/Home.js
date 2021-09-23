adf67a373aeb8a9ca0e57e1611caf7dbd48e0e4f
Abhisha Shah <abhisha5993@gmail.com>
2021-09-22 20:28:16 -0400

    handled errors and loading message untill response come from api

--- React Projects/blog-app/src/components/Home.js
+++ React Projects/blog-app/src/components/Home.js
@@ -3,20 +3,30 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null); // handle all blogdata using state
+    const [isPending,setPending] = useState(true) // to handle loading message
+    const [error,setError] = useState(null); // to handle errors
 
 // call rest api only once
 useEffect(()=>{
        fetch('http://localhost:3000/blogs')
         .then(res => {
+            if(!res.ok) throw Error("Not able to fetch the data");
             return(res.json());
         })
         .then(data => {
             setBlogs(data);
+            setPending(false);
+        })
+        .catch(err => {
+            setPending(false);
+            setError(err.message);
         })
     },[])
 
     return(
         <div className="home">
+            {error && <div> {error} </div>}
+            {isPending && "Loading..."}
             {blogs && <BlogList  blogs={blogs} />}
         </div>
     );
